fee4451bc98873ab67c851f16a64c851fd2f549e
lcalvin02 <lcalvin02@qub.ac.uk>
2024-08-09 17:20:33 +0100

    s

--- kolmogorov.py
+++ kolmogorov.py
@@ -28,7 +28,7 @@ with open("/Users/lukecalvin/2023/eli_np_muon_primaries_1.0GeV/muoncoeffsave.csv
         muoncoeff[1].append(float(line.split()[1]))
 
 
-print(muoncoeff)
+#print(muoncoeff)
 
 for i in range(0,len(eleccoeff[1])):
     for j in range(0,int(eleccoeff[1][i])):
@@ -59,8 +59,8 @@ print('coeff: ',ks_2samp(x, y))
 
 electrack=[[],[]]
 muontrack=[[],[]]
-x=[]
-y=[]
+a=[]
+b=[]
 with open("/Users/lukecalvin/2023/eli_np_muon_primaries_1.0GeV/electracksave.csv", "r") as txt_file3:
     for line in txt_file3:
         
@@ -77,18 +77,18 @@ with open("/Users/lukecalvin/2023/eli_np_muon_primaries_1.0GeV/muontracksave.csv
         muontrack[1].append(float(line.split()[1]))
 
 
-print(muontrack)
+#print(muontrack)
 
 for i in range(0,len(electrack[1])):
     for j in range(0,int(electrack[1][i])):
         print(int(electrack[1][i]))
-        x.append(electrack[0][i])
+        a.append(electrack[0][i])
 
 
 for i in range(0,len(muontrack[1])):
     for j in range(0,int(muontrack[1][i])):
         print(int(muontrack[1][i]))
-        y.append(muontrack[0][i])
+        b.append(muontrack[0][i])
 #print(y)
 
 

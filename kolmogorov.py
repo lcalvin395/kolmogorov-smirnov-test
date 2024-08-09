79c8df5130a41250c67c25f93a26f42f4f412aad
lcalvin02 <lcalvin02@qub.ac.uk>
2024-08-09 17:19:41 +0100

    s

--- kolmogorov.py
+++ kolmogorov.py
@@ -18,6 +18,7 @@ with open("/Users/lukecalvin/2023/eli_np_muon_primaries_1.0GeV/eleccoeffsave.csv
         eleccoeff[0].append(float(line.split()[0]))
         eleccoeff[1].append(float(line.split()[1]))
 
+
 #print(eleccoeff)
 
 with open("/Users/lukecalvin/2023/eli_np_muon_primaries_1.0GeV/muoncoeffsave.csv", "r") as txt_file2:
@@ -25,6 +26,7 @@ with open("/Users/lukecalvin/2023/eli_np_muon_primaries_1.0GeV/muoncoeffsave.csv
         
         muoncoeff[0].append(float(line.split()[0]))
         muoncoeff[1].append(float(line.split()[1]))
+
 
 print(muoncoeff)
 
@@ -47,11 +49,56 @@ np.random.seed(12345678)
 test = np.random.normal(0, 1, 1000)
 
 
-print(x)
-print(test)
+#print(x)
+#print(test)
 
-#print(ks_2samp(x, y))
+print('coeff: ',ks_2samp(x, y))
 #Ks_2sampResult(statistic=0.022999999999999909, pvalue=0.95189016804849647)
 #Ks_2sampResult(statistic=0.41800000000000004, pvalue=3.7081494119242173e-77)
 
 
+electrack=[[],[]]
+muontrack=[[],[]]
+x=[]
+y=[]
+with open("/Users/lukecalvin/2023/eli_np_muon_primaries_1.0GeV/electracksave.csv", "r") as txt_file3:
+    for line in txt_file3:
+        
+        electrack[0].append(float(line.split()[0]))
+        electrack[1].append(float(line.split()[1]))
+
+
+#print(electrack)
+
+with open("/Users/lukecalvin/2023/eli_np_muon_primaries_1.0GeV/muontracksave.csv", "r") as txt_file4:
+    for line in txt_file4:
+        
+        muontrack[0].append(float(line.split()[0]))
+        muontrack[1].append(float(line.split()[1]))
+
+
+print(muontrack)
+
+for i in range(0,len(electrack[1])):
+    for j in range(0,int(electrack[1][i])):
+        print(int(electrack[1][i]))
+        x.append(electrack[0][i])
+
+
+for i in range(0,len(muontrack[1])):
+    for j in range(0,int(muontrack[1][i])):
+        print(int(muontrack[1][i]))
+        y.append(muontrack[0][i])
+#print(y)
+
+
+
+
+np.random.seed(12345678)
+test = np.random.normal(0, 1, 1000)
+
+
+#print(x)
+#print(test)
+
+print('track: ',ks_2samp(x, y))
